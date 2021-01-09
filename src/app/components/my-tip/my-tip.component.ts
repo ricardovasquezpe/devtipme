6fb17c72f3a3b0a10234dfc299e9030f12405c12
Ricardo Vasquez <ricardo.vasquez@realplaza.com.pe>
2021-01-08 21:56:40 -0500

    adding modal

--- src/app/components/my-tip/my-tip.component.ts
+++ src/app/components/my-tip/my-tip.component.ts
@@ -1,5 +1,6 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-my-tip',
@@ -27,16 +28,35 @@ import { Component, OnInit } from '@angular/core';
     )
   ]
 })
-export class MyTipComponent implements OnInit {
+export class MyTipComponent {
 
-  constructor() { }
+  title = 'appBootstrap';
+  
+  closeResult: string;
+  
   showAddIcon: boolean = false;
-
-  ngOnInit(): void {
+  constructor(private modalService: NgbModal) {}
+    
+  open(content) {
+    this.modalService.open(content, {
+      ariaLabelledBy: 'modal-basic-title', 
+      size: 'sm', 
+      windowClass: 'add-modal'
+  }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
   }
-
-  clickTip(){
-
+  
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
   mouseOver(){
